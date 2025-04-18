b2c78d31f2a51fd46de4bdf4d0d6838b796d98bc
Garry <robertwinters767@gmail.com>
2025-04-18 14:25:36 -0400

    Fixed background, buttons look better, got a cool glass effect for questions

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,9 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-      <div class="bg-[url(./assets/images/kahoot.png)] h-full bg-cover bg-center">
-        <div className="w-fit h-24 mt-6 mx-auto rounded-lg text-[3rem] place-content-center px-8 bg-white/20 shadow-lg ring-1 ring-black/5">
+      <div className="relative min-h-screen pt-6">
+        <div className="absolute inset-0 -z-10 bg-cover bg-no-repeat bg-top bg-[url('./assets/images/kahoot.png')]"/>
+        <div className="w-fit h-24 mx-auto rounded-lg text-[3rem] place-content-center px-8 bg-white/40 shadow-lg ring-1 ring-black/5">
           hello, this is blerp, laallalalalal, gooby goop
         </div>
         <div className="absolute bg-transparent inset-x-0 bottom-0 h-[32.5rem] grid grid-cols-2 gap-4 p-4">
@@ -18,14 +19,14 @@ function App() {
               border-r-[51.96px] border-r-transparent">
             </div> 
           </div>
-          <div className="bg-[#1368CE] rounded-md place-content-center p-12">
+          <div className="bg-[#1368CE] rounded-md place-content-center p-12 h-[14.75rem]">
             <div class="w-[5.303rem] h-[5.303rem] bg-white rotate-45"></div>
           </div>
-          <div className="bg-[#D89E00] rounded-md place-content-center p-12">
+          <div className="bg-[#D89E00] rounded-md place-content-center p-12 h-[14.75rem]">
             <div class="w-[120px] h-[120px] bg-white rounded-full">
             </div> 
           </div>
-          <div className="bg-[#26890C] rounded-md place-content-center p-12">
+          <div className="bg-[#26890C] rounded-md place-content-center p-12 h-[14.75rem]">
             <div class="w-[5.303rem] h-[5.303rem] bg-white"></div>
           </div>
         </div>
